7ccb40cf4de1164f5af35a7d2fe6a8c9ad39b06d
Atom Yah <atom@yah>
2017-12-27 16:47:07 +0900

    p188修正

--- line_callback.php
+++ line_callback.php
@@ -24,23 +24,23 @@ $csrf_token = $session->getCsrfToken();
 
 //リクエストの種類とトークンの同一性を検証
 if ($unsafe || !$csrf_token->isValid($csrf_value)) {
-  echo '<p>不正なリクエストです</p>';
+  echo '<p>不正なリクエストです。</p>';
   return;
 }
 
 // LINEのサーバーでログイン処理ごとにGETアクセスされるページ
 $callback = 'https://' . $_SERVER['HTTP_HOST'] . '/line_callback.php';
-// ログイン成功時はパラメータにcodeが付与付与されている
+// ログイン成功時はパラメータにcodeが付与されている
 if (isset($_GET['code'])) {
   // APIへのアクセストークンを取得するエンドポイント
   $url = 'https://api.line.me/v2/oauth/accessToken';
   //データ
   $data = array(
     'grant_type' => 'authorization_code',
-     'client_id' => getenv('LOGIN_CHANNEL_ID'),
-     'client_secret' => getenv('LOGIN_CHANNEL_SECRET'),
-     'code' => $_GET['code'],
-     'redirect_uri' => $callback
+    'client_id' => getenv('LOGIN_CHANNEL_ID'),
+    'client_secret' => getenv('LOGIN_CHANNEL_SECRET'),
+    'code' => $_GET['code'],
+    'redirect_uri' => $callback
   );
   $data = http_build_query($data, '', '&');
   //ヘッダー
@@ -82,7 +82,7 @@ if (isset($_GET['code'])) {
     // ユーザーにメッセージを送信
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-    $bot->pushMessage($profile["userID"], new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ブラウザ経由でログインしました'));
+    $bot->pushMessage($profile["userId"], new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ブラウザ経由でログインしました'));
     
   }
   
@@ -90,7 +90,7 @@ if (isset($_GET['code'])) {
 
 //ログイン失敗時
 else {
-  echo 'ログイン失敗です';
+  echo '<p>ログイン失敗しました。</p>';
 }
         
 ?>        
